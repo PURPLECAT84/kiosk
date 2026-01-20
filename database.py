d8721a15c7e28029426847180ceefeefb5ff1c8d
PURPLECAT84 <pmountain@naver.com>
2026-01-20 22:57:21 +0900

    database 세팅 완료

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DB_URL = "postgresql://postgres.wmrnknlikvcohczmyvpb:HwIpn08gBh9gi8@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+# supabase 비번 : PGHwIpn08gBh9gi8
+
+engine = create_engine(DB_URL, pool_pre_ping = True)
+#pool_pre_ping = True : DB 와의 연결 전 매번 연결 상태 체크 실행
+
+DB_session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+"""
+bind = engine <- 세션과 DB 주소가 있는 engine 연결
+autocommit <-commit()이라고 말하기 전까지는 절대 DB에 영구 저장하지 마!, True 는 자동저장
+autoflush <- 자동 명령 전송 끄기, 불필요한 DB 전송 및 액션 감소, 성능 향상
+"""
+
+class Base(DeclarativeBase):
+    pass
+#get_db 는 추후 main.py 에서 작성
---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+[라이브러리 설명]
+
+**"보안(Security)"**과 "인증(Auth)" 기능을 만들 때 없어서는 안 될 3대장
+
+1. passlib[bcrypt] : 🔒 비밀번호 금고지기
+
+역할: 비밀번호 해싱(Hashing) 라이브러리
+비유: "문서 파쇄기 + 금고"
+
+
+2. python-jose : 🆔 출입증 발급기
+역할: JWT(JSON Web Token) 생성 및 관리 라이브러리
+비유: "위조 방지 도장이 찍힌 사원증 발급 기계"
+
+3. python-multipart : 📦 택배 상자 번역기
+역할: Form Data 파싱(해석) 라이브러리
+비유: "폼(Form) 언어 통역사"
+OAuth2PasswordRequestForm을 사용하려면 이 라이브러리가 필수
+"""
