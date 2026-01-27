8426c0e9a051382bd94adda8c4e5bc0dad8a8e11
PURPLECAT84 <pmountain@naver.com>
2026-01-27 22:52:10 +0900

    로그인(토큰발행) 기능업데이트

--- routers/users.py
+++ routers/users.py
@@ -4,16 +4,18 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from models import User
-from schemas import UserCreate, UserResponse
+from schemas import UserCreate, UserResponse, UserLogin, Token
+from security import get_password_hash, verify_password, create_access_token
 
-from security import get_password_hash
+"""=====================여기서 부터 해싱(암호화) 코드(JWT)============================"""
+
 
 router = APIRouter(prefix = "/users", tags = ["users"])
 
 @router.post("/signup", response_model = UserResponse, status_code = status. HTTP_201_CREATED, summary = "회원가입", description = "회원 정보 입력")
 def create_user(user : UserCreate, db : Session = Depends(get_db)):
-    stmt = select(User).where(User.email == user.email)
-    existing_user = db.execute(stmt).scalars().first()
+    stmt = select(User).where(User.email == user.email) #???
+    existing_user = db.execute(stmt).scalars().first() #???
 
     if existing_user:
         raise HTTPException(status_code = status.HTTP_409_CONFLICT, detail = "이미 존재하는 이메일입니다")
@@ -34,3 +36,18 @@ def create_user(user : UserCreate, db : Session = Depends(get_db)):
 
     return new_user
 
+"""=====================여기서 부터 토큰 코드(JWT)============================"""
+
+@router.post("/login",response_model = Token)
+def token_access(user_login : UserLogin, db: Session = Depends(get_db)):
+    stmt = select(User).where(User.email == user_login.email)
+    user = db.execute(stmt).scalars().first()
+
+    if not user or not verify_password(user_login.password, user.password):
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, 
+                            detail = "이메일 또는 비밀번호가 일치하지 않습니다", 
+                            headers={"WWW-Authenticate": "Bearer"}) #???
+    
+    access_token = create_access_token(data = {"sub": user.email})
+
+    return {"access_token" : access_token, "token_type" : "bearer"} #???
--- security.py
+++ security.py
@@ -1,5 +1,10 @@
 from passlib.context import CryptContext
 
+from datetime import datetime, timedelta, timezone
+import jwt
+
+"""=====================여기서 부터 해싱(암호화) 코드============================"""
+
 pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto") #암호화 알고리즘
 
 def get_password_hash(password : str) -> str :
@@ -7,3 +12,21 @@ def get_password_hash(password : str) -> str :
 
 def verify_password(original_password : str, hashed_password : str) -> bool:
     return pwd_context.verify(original_password, hashed_password)
+
+
+"""=====================여기서 부터 토큰 코드(JWT)============================"""
+
+SECRET_KEY = "User_Authority_Key"
+ALGORITHM = "HS256" # 암호화 알고리즘
+ACCESSABLE_TIME = 30
+
+def create_access_token(data: dict):
+    copied_data = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESSABLE_TIME)
+
+    copied_data.update({"expired": expire.timestamp()})
+
+    encoded_jwt = jwt.encode(copied_data, SECRET_KEY, algorithm = ALGORITHM)
+
+    return encoded_jwt
