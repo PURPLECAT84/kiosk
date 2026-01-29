c39d20e8e2946c896f07f23114c815064f80dbd0
PURPLECAT84 <pmountain@naver.com>
2026-01-29 22:33:52 +0900

    토큰 검증 조회 기능 완성

--- routers/users.py
+++ routers/users.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from database import get_db
 from models import User
 from schemas import UserCreate, UserResponse, UserLogin, Token
-from security import get_password_hash, verify_password, create_access_token
+from security import get_password_hash, verify_password, create_access_token, ALGORITHM, SECRET_KEY
 
-"""=====================여기서 부터 해싱(암호화) 코드(JWT)============================"""
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+"""=====================여기서 부터 해싱(암호화) 코드============================"""
 
 
 router = APIRouter(prefix = "/users", tags = ["users"])
@@ -39,11 +42,12 @@ def create_user(user : UserCreate, db : Session = Depends(get_db)):
 """=====================여기서 부터 토큰 코드(JWT)============================"""
 
 @router.post("/login",response_model = Token)
-def token_access(user_login : UserLogin, db: Session = Depends(get_db)):
-    stmt = select(User).where(User.email == user_login.email)
+def token_access(form_data : OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    stmt = select(User).where(User.email == form_data.username)
     user = db.execute(stmt).scalars().first()
 
-    if not user or not verify_password(user_login.password, user.password):
+   
+    if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, 
                             detail = "이메일 또는 비밀번호가 일치하지 않습니다", 
                             headers={"WWW-Authenticate": "Bearer"}) #???
@@ -51,3 +55,35 @@ def token_access(user_login : UserLogin, db: Session = Depends(get_db)):
     access_token = create_access_token(data = {"sub": user.email})
 
     return {"access_token" : access_token, "token_type" : "bearer"} #???
+
+"""=====================여기서 부터 토큰 검증 및 조회============================"""
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/users/login")
+
+def get_current_user(token : str = Depends(oauth2_scheme), db : Session = Depends(get_db)) ->User:
+
+    credentials_exception = HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = "로그인 정보를 확인할 수 없습니다",
+                                          headers = {"WWW-Authenticate" : "Bearer"},)
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm = [ALGORITHM]) # 토큰 해독 (Decode) # SECRET_KEY로 서명을 확인하고, 만료 시간(exp)도 자동으로 체크
+
+        email : str | None = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+    except InvalidTokenError :
+        credentials_exception
+
+    stmt = select(User).where(User.email == email)
+    user = db.execute(stmt).scalars().first()
+
+    if user is None :
+        raise credentials_exception
+    
+    return user
+
+@router.get ("/me", response_model = UserResponse, summary = "회원검증")
+
+def read_users_me(current_user : User = Depends(get_current_user)):
+    
+    return current_user
