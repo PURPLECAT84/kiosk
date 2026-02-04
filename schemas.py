2fbf2c93a0b2a5301b57b887a457002d864ffce2
PURPLECAT84 <pmountain@naver.com>
2026-02-04 22:26:01 +0900

    스키마 작성 완료

--- schemas.py
+++ schemas.py
@@ -43,3 +43,97 @@ class Token(BaseModel):
     access_token : str
     token_type : str
 
+"""============여기부터 매장-매대-카테고리-상품 관련 스키마=============="""
+
+
+class StoreCreate(BaseModel):
+
+    name : str
+    address : str
+    type : str | None = None
+
+
+class StoreUpdate(BaseModel):
+
+    name : str | None = None
+    address : str | None = None
+    type : str | None = None
+    
+class StoreResponse(BaseModel):
+
+    id: uuid.UUID
+    name : str
+    address: str
+    type : str
+    created_date : datetime
+
+    model_config = ConfigDict(from_attributes = True)
+
+class ShelveCreate(BaseModel):
+
+    name : str
+    terminal_id : str
+    business_number : str 
+    vender_code : str 
+
+class ShelveUpdate(BaseModel):
+
+    name : str | None = None
+    terminal_id : str | None = None
+    business_number : str | None = None 
+    vender_code : str | None = None
+
+class ShelveResponse(BaseModel):
+
+    id: uuid.UUID
+    store_id : uuid.UUID
+    name : str
+    terminal_id : str
+    business_number : str
+    vender_code : str
+
+    model_config = ConfigDict(from_attributes = True)
+
+
+class CategoryCreate(BaseModel):
+    
+    name : str
+
+class CategoryUpdate(BaseModel):
+
+    name : str | None = None
+
+class CategoryResponse(BaseModel):
+
+    id : int
+    name : str
+    shelve_id : uuid.UUID
+
+    model_config = ConfigDict(from_attributes = True)
+
+class ProductCreate(BaseModel):
+
+    barcode : str | None = None
+    name : str
+    price : int
+    buy_from : str | None = None
+
+class ProductUpdate(BaseModel):
+
+    barcode : str | None = None
+    name : str | None = None
+    price : int | None = None
+    buy_from : str | None = None
+
+
+class ProductResponse(BaseModel):
+
+    id : uuid.UUID
+    category_id : int
+    barcode : str | None = None
+    name : str
+    price : int
+    buy_from : str | None = None
+    created_date :datetime
+
+    model_config = ConfigDict(from_attributes = True)
